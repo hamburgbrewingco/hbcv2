4e29b6c8ee947bd33103206fbf7359008a1e96e8
Vergetone Productions <info@vergetone.com>
2013-12-31 10:56:48 -0500

    Protect beers, locations, and features pages

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,6 @@
 class BeersController < ApplicationController
   before_action :set_beer, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @beers = Beer.all
