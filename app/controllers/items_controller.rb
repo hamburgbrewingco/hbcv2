4abad8f58b8bff801280b3e47e30d9609026b2b5
Vergetone Productions <info@vergetone.com>
2014-01-13 22:36:26 -0500

    Add text description to items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -70,6 +70,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :price, :description, :category, :alcohol_percent)
+      params.require(:item).permit(:name, :price, :description, :category, :alcohol_percent, :description_fix)
     end
 end
