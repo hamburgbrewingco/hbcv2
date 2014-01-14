2012d66935fe7644daca52e30a2f42d1fcfe3a82
Vergetone Productions <info@vergetone.com>
2014-01-13 22:46:04 -0500

    Update menu order by created at asc

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.order('created_at ASC')
   end
 
   # GET /items/1
